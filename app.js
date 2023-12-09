cb0e3385df4bb2ed97ecdef166d802feb25b216c
ycluis <ycluisdev@gmail.com>
2023-12-09 21:47:27 +0800

    feat(app.js): import todoist sdk

--- app.js
+++ app.js
@@ -2,22 +2,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import axios from "axios";
+import { TodoistApi } from "@doist/todoist-api-typescript";
+
+const api = new TodoistApi(process.env.TODOIST_API);
 
 (async () => {
   try {
-    const res = await axios.get(process.env.URL, {
-      headers: {
-        Accept: "application/json",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
-      },
-    });
+    // const res = await axios.get(process.env.URL, {
+    //   headers: {
+    //     Accept: "application/json",
+    //     "User-Agent":
+    //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    //   },
+    // });
+    // if (res.status === 200 && res.data.success) {
+    //   console.log(res.data.result.records);
+    // } else {
+    //   console.log("Failed to fetch the website");
+    // }
 
-    if (res.status === 200 && res.data.success) {
-      console.log(res.data.result.records);
-    } else {
-      console.log("Failed to fetch the website");
-    }
+    //* TODOIST API
+    api
+      .getTasks()
+      .then((tasks) => console.log(tasks))
+      .catch((err) => console.log(err));
   } catch (err) {
     console.log(err);
   }
